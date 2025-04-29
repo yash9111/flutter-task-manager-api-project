0fced555613ead0c2c2488242e4a38756be38c4a
Ragib Shahariar <ragibshahariar@Ragibs-Mac-mini.local>
2025-04-29 20:44:25 +0600

    project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_manager_api_project/app.dart';
 
+//Rafat vai I was going through semester final exam
+//several live class i have missed for that reason
+//Sorry for late submission
+
 void main() {
   runApp(const MyApp());
 }
